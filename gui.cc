0424af67c148a47db419abef64116f9bc37cbc8c
gunyagisa <gunyagisa@gmail.com>
2020-09-28 16:28:27 +0900

    add GUI Class

---
+++ gui.cc
@@ -0,0 +1,106 @@
+#include <bits/stdint-uintn.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <GL/glut.h>
+
+#include <sys/wait.h>
+#include <thread>
+
+class GUI {
+    unsigned int width, height;
+    uint8_t *bitmap;
+
+  public:
+    uint8_t *vram;
+    GUI() {};
+    GUI(unsigned int width, unsigned int height, uint8_t *vram)
+    {
+      Init(width, height, vram);
+    }
+
+    ~GUI() {}
+
+    void Init(int width, int height, uint8_t *vram)
+    {
+      this->width = width;
+      this->height = height;
+      this->vram = vram;
+      bitmap = (unsigned char *) calloc(width * height * 3, sizeof(unsigned char));
+    }
+
+
+    void convert2(const unsigned char *palette)
+    {
+      printf("start convert\n");
+      unsigned char rgb[3];
+      int x = 0;
+      int y = 0;
+      for (int i = 0x0000; i <= 0xffff; i++) {
+        fflush(stdout);
+        uint8_t col = vram[i];
+        fflush(stdout);
+        rgb[0] = palette[col * 3];
+        rgb[1] = palette[col * 3 + 1];
+        rgb[2] = palette[col * 3 + 2];
+        
+        int where = (y * width + x) * 3;
+        bitmap[where] = rgb[0];
+        bitmap[where + 1] = rgb[1];
+        bitmap[where + 2] = rgb[2];
+
+        x++;
+        if (x >= width) {
+          x = 0;
+          y++;
+        }
+      }
+    }
+
+    unsigned int get_width() { return width; }
+    unsigned int get_height() { return height; }
+    unsigned char * get_bitmap() { return bitmap; }
+};
+
+GUI gui;
+uint8_t palette[] = {
+  0x00, 0x00, 0x00,
+  0xff, 0x00, 0x00,
+  0x00, 0xff, 0x00,
+  0xff, 0xff, 0x00,
+  0x00, 0x00, 0xff,
+  0xff, 0x00, 0xff,
+  0x00, 0xff, 0xff,
+  0xff, 0xff, 0xff,
+  0xc6, 0xc6, 0xc6,
+  0x84, 0x00, 0x00,
+  0x00, 0x84, 0x00,
+  0x84, 0x84, 0x84,
+  0x00, 0x00, 0x84,
+  0x84, 0x00, 0x84,
+  0x00, 0x84, 0x84,
+  0x84, 0x84, 0x84,
+};
+
+
+void disp()
+{
+  printf("start display\n");
+  gui.convert2(palette);
+  glClear(GL_COLOR_BUFFER_BIT);
+  //glRasterPos2i(-1 , -1);
+  glDrawPixels(gui.get_width() , gui.get_height() , GL_RGB , GL_UNSIGNED_BYTE , gui.get_bitmap());
+  glFlush();
+}
+
+void start(GUI &gui, int *argc, char **argv)
+{
+  printf("%x\n", gui.vram);
+  glutInit(argc, argv);
+  glutInitWindowSize(gui.get_width(), gui.get_height());
+  glutInitDisplayMode(GLUT_RGB);
+  glutCreateWindow("GUI TEST");
+  glutDisplayFunc(disp);
+  glutMainLoop();
+}
+
